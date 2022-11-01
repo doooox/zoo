965ade502bbf9229ae6e4ff193d4cead8788591d
doooox <dtopic12@gmail.com>
2022-11-01 19:29:21 +0100

    if name unknown set nepoznt, and fixed table

--- src/components/AnimalList.js
+++ src/components/AnimalList.js
@@ -32,25 +32,40 @@ const AnimalList = () => {
             species: "Lion",
             name: "Simba",
             dateOfBirth: date
-        }
+        },
+        {
+            id: 6,
+            species: "Parrot",
+            name: "Wings",
+            dateOfBirth: ''
+        },
     ];
     console.log(animals);
     return (
         <div>
-            <table >
-                {animals.map((animal) =>
-                (
-                    <tr key={animal.id}>
-                        <td>{animal.species}</td>
-                        <td>{animal.name}</td>
-                        <td>{animal.dateOfBirth}</td>
+            <table>
+                <thead>
+                    <tr>
+                        <th>species</th>
+                        <th>name</th>
+                        <th>Date of Birth</th>
+            
                     </tr>
-                )
-                )}
+                </thead>
+                <tbody>
+                    {animals.map((animal) =>
+                    (
+                        <tr key={animal.id}>
+                            <td>{animal.species}</td>
+                            <td>{animal.name}</td>
+                            <td>{animal.dateOfBirth ? animal.dateOfBirth : "Nepoznat"}</td>
+                        </tr>
+                    )
+                    )}
+                </tbody>
             </table>
         </div>
     )
-
 
 }
 
